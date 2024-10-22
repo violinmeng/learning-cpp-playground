0eeef66b8bc6d6f496e54087e738b7414bd52a3c
Elin Wang <violinmeng@gmail.com>
2024-10-22 23:31:03 +0800

    Add copy initializer code.

--- bin/main/main.cpp
+++ bin/main/main.cpp
@@ -4,6 +4,7 @@
 
 #include "metaprogramming.hpp"
 #include "const.hpp"
+#include "copyinitializer.hpp"
 
 using namespace std;
 int main()
@@ -13,6 +14,7 @@ int main()
     MetaProgramming::metaProgrammingClientCode();
     MetaProgramming::computationOnTypes();
     LearnConst::testConst();
+    CopyInitializer::testCopyConstructor();
     return 0;
   } catch (const std::exception &e) {
     std::cerr << "Error: " << e.what() << "\n";
